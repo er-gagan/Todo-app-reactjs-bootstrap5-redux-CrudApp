1ceb82e8fd64133dfa269a03818599dd0882d471
Gagan Aggarwal <gaggarwal124@gmail.com>
2021-05-20 23:49:57 +0530

    Some bug fixed
    Some bug fixed

--- todo/src/components/home/Card.js
+++ todo/src/components/home/Card.js
@@ -3,35 +3,36 @@ import { useSelector, useDispatch } from 'react-redux'
 import { deleteTodo, updateTodo } from '../../actions';
 
 const Card = () => {
-
     const [id, setId] = useState('')
     const [title, setTitle] = useState('')
     const [desc, setDesc] = useState('')
 
     const todos = useSelector((state) => state.todos.data);
     const dispatch = useDispatch()
-    const openbtn = (ids, titles, descs) => {
-        setId(ids)
-        setTitle(titles)
-        setDesc(descs)
+
+    const openbtn = (id, title, desc) => {
+        setId(id)
+        setTitle(title)
+        setDesc(desc)
     }
+
     const todoUpdate = () => {
-        let newId = document.getElementById("newId").value
         let newTitle = document.getElementById("editTitle").value
         let newDesc = document.getElementById("editDesc").value
-        dispatch(updateTodo({ id: newId, title: newTitle, desc: newDesc }))
+        dispatch(updateTodo({ id: id, title: newTitle, desc: newDesc }))
     }
 
     if (todos.length) {
         const taskItems = todos.map((item) => {
             return (
                 <div key={item.id} className="col-md-3">
-                    <div className="card my-2" style={{ width: "16rem" }}>
+                    <div className="card my-2 shadow bg-body rounded" style={{ width: "16rem" }}>
                         <div className="card-body">
-                            <h5 className="card-title">{item.title}</h5>
+                            <h5 className="card-title">{(item.title.length > 15) ? item.title.slice(0, 15) + "..." : item.title}</h5>
                             <hr />
                             <h6 className="card-subtitle mb-2 text-muted">Description</h6>
-                            <p className="card-text" style={{ textAlign: 'justify' }}>{item.desc}</p>
+                            <p className="card-text" style={{ textAlign: 'justify' }}>{(item.desc.length > 100) ? item.desc.slice(0, 100) + "..." : item.desc}</p>
+
                             {/* Modal for open button start */}
                             <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                                 <div className="modal-dialog">
@@ -41,8 +42,6 @@ const Card = () => {
                                             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                         </div>
                                         <div className="modal-body">
-                                            <input type="text" id="newId" onChange={(e) => setId(e.target.value)} value={id} />
-
                                             <div className="mb-3">
                                                 <label htmlFor="editTitle" className="form-label">Title</label>
                                                 <input type="text" onChange={(e) => setTitle(e.target.value)} className="form-control" id="editTitle" value={title} />
@@ -50,7 +49,7 @@ const Card = () => {
 
                                             <div className="mb-3">
                                                 <label htmlFor="editDesc" className="form-label">Description</label>
-                                                <textarea type="text" className="form-control" id="editDesc" onChange={(e) => setDesc(e.target.value)} value={desc} ></textarea>
+                                                <textarea type="text" className="form-control" id="editDesc" onChange={(e) => setDesc(e.target.value)} value={desc} rows="5" ></textarea>
                                             </div>
                                         </div>
                                         <div className="modal-footer">
@@ -61,8 +60,10 @@ const Card = () => {
                                 </div>
                             </div>
                             {/* Modal for open button end */}
+
                             <div className="text-center">
                                 <button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal" className="btn btn-info btn-sm" onClick={() => openbtn(item.id, item.title, item.desc)}>Open</button>
+
                                 <button className="btn btn-warning btn-sm mx-3" onClick={() => dispatch(deleteTodo(item.id))}>Delete</button>
                             </div>
                         </div>
