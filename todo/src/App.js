a8cf6622eee8397eec83d3ab907110281aacad01
Gagan Aggarwal <gaggarwal124@gmail.com>
2021-05-22 12:18:28 +0530

    React Routing complete
    React Routing complete

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,22 +1,33 @@
-// import logo from './logo.svg';
-// import './App.css';
 import Navbar from './components/navbar/Navbar';
-// import Home from './components/home/Home';
-// import Login from './components/Login/Login';
-// import Signup from './components/Signup/Signup';
-// import ChangePassword from './components/ChangePassword/ChangePassword';
+import Home from './components/home/Home';
+import Login from './components/Login/Login';
+import Signup from './components/Signup/Signup';
+import ChangePassword from './components/ChangePassword/ChangePassword';
 import ForgotPassword from './components/ForgotPassword/ForgotPassword';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <Router>
       <Navbar />
-      {/* <Home /> */}
-      {/* <Login /> */}
-      {/* <Signup /> */}
-      {/* <ChangePassword /> */}
-      <ForgotPassword />
-    </div>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/signup">
+          <Signup />
+        </Route>
+        <Route exact path="/changePassword">
+          <ChangePassword />
+        </Route>
+        <Route exact path="/forgotPassword">
+          <ForgotPassword />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- todo/src/components/navbar/Navbar.js
+++ todo/src/components/navbar/Navbar.js
@@ -1,46 +1,47 @@
 import React from 'react'
 import userImage from './user.png'
+import { Link } from "react-router-dom";
 const Navbar = () => {
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="/">Home</a>
+                    <Link className="navbar-brand" to="/">Home</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             {/* <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                                <Link className="nav-link active" aria-current="page" to="#">Home</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Link</a>
+                                <Link className="nav-link" to="#">Link</Link>
                             </li>
                             <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <Link className="nav-link dropdown-toggle" to="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Dropdown
-                                </a>
+                                </Link>
                                 <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a className="dropdown-item" href="#">Action</a></li>
-                                    <li><a className="dropdown-item" href="#">Another action</a></li>
+                                    <li><Link className="dropdown-item" to="#">Action</Link></li>
+                                    <li><Link className="dropdown-item" to="#">Another action</Link></li>
                                     <li><hr className="dropdown-divider" /></li>
-                                    <li><a className="dropdown-item" href="#">Something else here</a></li>
+                                    <li><Link className="dropdown-item" to="#">Something else here</Link></li>
                                 </ul>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                                <Link className="nav-link disabled" to="#" tabindex="-1" aria-disabled="true">Disabled</Link>
                             </li> */}
                         </ul>
                         <div className="btn-group">
-                            <a href="/" className="dropdown" data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">
+                            <Link to="/" className="dropdown" data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">
                                 <img style={{ width: "40px", border: "2px solid white", borderRadius: "50%", backgroundColor: "white" }} src={userImage} alt="NoImage" />
-                            </a>
+                            </Link>
                             <ul className="dropdown-menu dropdown-menu-lg-end dropdown-menu-dark">
-                                <li><button className="dropdown-item" type="button">Login</button></li>
-                                <li><button className="dropdown-item" type="button">Signup</button></li>
-                                <li><button className="dropdown-item" type="button">Change Password</button></li>
-                                <li><button className="dropdown-item" type="button">Forgot Password</button></li>
+                                <li><Link className="dropdown-item" to="/login">Login</Link></li>
+                                <li><Link className="dropdown-item" to="/signup">Signup</Link></li>
+                                <li><Link className="dropdown-item" to="/changePassword">Change Password</Link></li>
+                                <li><Link className="dropdown-item" to="/forgotPassword">Forgot Password</Link></li>
                             </ul>
                         </div>
                     </div>
